7973bb8e90fb15c3b24a477e9580467e71cb377f
HIMAR MANUEL BARQUIN CARRASCO <alu0101119373@ull.edu.es>
2020-03-04 17:20:56 +0000

    Terminado compilador

--- compiler/assembler.py
+++ compiler/assembler.py
@@ -36,27 +36,42 @@ REGISTER = {
     "R15": "1111"
 }
 
-def formatBinaryInstruction(instruction):
+def formatBinaryInstruction(bInstruction):
     result = ""
-    if len(instruction) == 16:
-        result = instruction[:4] + "_" + instruction[4:8] + "_" + instruction[8:12] + "_" + instruction[12:16]
+    if len(bInstruction) == 16:
+        result = bInstruction[:4] + "_" + bInstruction[4:8] + "_" + bInstruction[8:12] + "_" + bInstruction[12:16]
     else:
         print("ERROR!")
     return result
 
-def inmTo8bit (inmediate):
-    return "{:b}".format(inmediate).zfill(8)
+def inmToBit (bit, inmediate):
+    return "{:b}".format(int(inmediate)).zfill(bit)
 
-def toBinary (data):
-    if data in REGISTER:
-        return REGISTER[data]
-    elif data.isdigit():
-        return inmTo8bit(int(data))
-    else:
-        return "{} is not a valid data".format(data)
+def divideInstruction (instruction):
+    return [ word.strip() for word in instruction.split() if len(word.strip()) != 0 ]
 
-def process (line):
-    instruction = line.split()
+def isComplex (instruction):
+    return instruction[0] in COMPLEX
+
+def analyzeComplexInstruction (instruction):
+    instance = ""
+
+    if instruction[0] == "BEQ":
+        instance = BEQ()
+    elif instruction[0] == "BNE":
+        instance = BNE()
+    elif instruction[0] == "BLT":
+        instance = BLT()
+    elif instruction[0] == "BLE":
+        instance = BLE()
+    elif instruction[0] == "BGT":
+        instance = BLE()
+    elif instruction[0] == "BGE":
+        instance = BLE()
+
+    return instance
+
+def process (instruction):
     if instruction[0] in PARSER:
         opcode = PARSER[instruction[0]]
         if opcode[:2] == "10":
@@ -76,10 +91,7 @@ def process (line):
             return AluInstruction(instruction[0], instruction[1], instruction[2], instruction[3])
         else:
             print("ERROR! Unknown error. That's pretty bad :(")
-            exit()
-    elif instruction[0] in COMPLEX:
-        opcode = instruction[0]
-        
+            exit()        
     else:
         print("ERROR! Instruction not recognized")
 
@@ -88,6 +100,12 @@ class Instruction:
 
     def __init__(self, opcode):
         self.opcode = opcode
+
+    def __str__ (self):
+        return self.opcode
+
+    def toBinary (self):
+        pass
 
 class JumpInstruction (Instruction):
     """Instrucciones de salto"""
@@ -99,6 +117,9 @@ class JumpInstruction (Instruction):
     def __str__(self):
         return "{} {}".format(self.opcode, self.dir)
 
+    def toBinary (self):
+        return PARSER[self.opcode] + inmToBit(10, self.dir)
+
 class InmediateInstruction (Instruction):
     def __init__ (self, opcode, inm, dest):
         super().__init__(opcode)
@@ -107,6 +128,9 @@ class InmediateInstruction (Instruction):
 
     def __str__(self):
         return "{} {} {}".format(self.opcode, self.inm, self.dest)
+    
+    def toBinary (self):
+        return PARSER[self.opcode] + inmToBit(8, self.inm) + REGISTER[self.dest]
 
 class AluInstruction (Instruction):
     def __init__ (self, opcode, r1, r2, rd):
@@ -118,70 +142,73 @@ class AluInstruction (Instruction):
     def __str__ (self):
         return "{} {} {} {}".format(self.opcode, self.r1, self.r2, self.rd)
 
+    def toBinary (self):
+        return PARSER[self.opcode] + REGISTER[self.r1] + REGISTER[self.r2] + REGISTER[self.rd]
+
 # Complex Instructions
+class ComplexContext:
+
+    def __init__ (self):
+        self.cInstruction = ComplexInstruction()
+
+    def setInstruction (self, instructionClass):
+        self.cInstruction = instructionClass
+
+    def toBinary (self, instruction):
+        return self.cInstruction.toBinary(instruction)
+
+    def getNativeInstructions (self, instruction):
+        if type(instruction) == list:
+            return self.cInstruction.getNativeInstructions(instruction)
+
 class ComplexInstruction:
-    def __init__(self, opcode):
-        self.opcode = opcode
+    def __init__(self):
+        pass
 
-class ConditionalInstruction (ComplexInstruction):
-    def __init__(self, opcode, r1, r2, dir):
-        super().__init__(opcode)
-        self.r1 = r1
-        self.r2 = r2
-        self.dir = dir
+    def toBinary (self, instruction):
+        nativeInstructions = self.getNativeInstructions(instruction)
+        binNativeInstructions = []
+        for ins in nativeInstructions:
+            binNativeInstructions.append(ins.toBinary())
+        return binNativeInstructions
 
-class BEQ (ConditionalInstruction):
-    def __init__ (self, opcode, r1, r2, dir):
-        super().__init__(opcode, r1, r2, dir)
+    def getNativeInstructions (self, instruction):
+        return instruction
 
-    def getNativeInstructions (self):
-        resta = AluInstruction("SUB", self.r1, self.r2, "R15")
-        salto = JumpInstruction("JZ", self.dir)
+class BEQ (ComplexInstruction):
+    def getNativeInstructions (self, instruction):
+        resta = AluInstruction("SUB", instruction[1], instruction[2], "R0")
+        salto = JumpInstruction("JZ", instruction[3])
         return [resta, salto]
 
-class BNE (ConditionalInstruction):
-    def __init__ (self, opcode, r1, r2, dir):
-        super().__init__(opcode, r1, r2, dir)
-
-    def getNativeInstructions (self):
-        resta = AluInstruction("SUB", self.r1, self.r2, "R15")
-        salto = JumpInstruction("JNZ", self.dir)
+class BNE (ComplexInstruction):
+    def getNativeInstructions (self, instruction):
+        resta = AluInstruction("SUB", instruction[1], instruction[2], "R15")
+        salto = JumpInstruction("JNZ", instruction[3])
         return [resta, salto]
 
-class BLT (ConditionalInstruction):
-    def __init__ (self, opcode, r1, r2, dir):
-        super().__init__(opcode, r1, r2, dir)
-
-    def getNativeInstructions (self):
-        resta = AluInstruction("SUB", self.r1, self.r2, "R15")
-        salto = JumpInstruction("JN", self.dir)
+class BLT (ComplexInstruction):
+    def getNativeInstructions (self, instruction):
+        resta = AluInstruction("SUB", instruction[1], instruction[2], "R15")
+        salto = JumpInstruction("JN", instruction[3])
         return [resta, salto]
 
-class BLE (ConditionalInstruction):
-    def __init__ (self, opcode, r1, r2, dir):
-        super().__init__(opcode, r1, r2, dir)
-
-    def getNativeInstructions (self):
-        resta = AluInstruction("SUB", self.r1, self.r2, "R15")
-        salto1 = JumpInstruction("JN", self.dir)
-        salto2 = JumpInstruction("JZ", self.dir)
+class BLE (ComplexInstruction):
+    def getNativeInstructions (self, instruction):
+        resta = AluInstruction("SUB", instruction[1], instruction[2], "R15")
+        salto1 = JumpInstruction("JN", instruction[3])
+        salto2 = JumpInstruction("JZ", instruction[3])
         return [resta, salto1, salto2]
 
-class BGT (ConditionalInstruction):
-    def __init__ (self, opcode, r1, r2, dir):
-        super().__init__(opcode, r1, r2, dir)
-
-    def getNativeInstructions (self):
-        resta = AluInstruction("SUB", self.r2, self.r1, "R15")
-        salto = JumpInstruction("JN", self.dir)
+class BGT (ComplexContext):
+    def getNativeInstructions (self, instruction):
+        resta = AluInstruction("SUB", instruction[2], instruction[1], "R15")
+        salto = JumpInstruction("JN", instruction[3])
         return [resta, salto]
 
-class BGE (ConditionalInstruction):
-    def __init__ (self, opcode, r1, r2, dir):
-        super().__init__(opcode, r1, r2, dir)
-
-    def getNativeInstructions (self):
-        resta = AluInstruction("SUB", self.r2, self.r1, "R15")
-        salto1 = JumpInstruction("JN", self.dir)
-        salto2 = JumpInstruction("JZ", self.dir)
+class BGE (ComplexContext):
+    def getNativeInstructions (self, instruction):
+        resta = AluInstruction("SUB", instruction[2], instruction[1], "R15")
+        salto1 = JumpInstruction("JN", instruction[3])
+        salto2 = JumpInstruction("JZ", instruction[3])
         return [resta, salto1, salto2]
--- compiler/compilador2.py
+++ compiler/compilador2.py
@@ -19,11 +19,29 @@ if not path.exists(inFile):
 
 # Leemos el fichero
 with open(inFile) as iFile:
+    oFile = open(outFile, "w")
     for line in iFile:
         fline = line.strip()
+        print("Processing instruction: {:15}, ".format(fline), end=" ")
         # Procesamos la instruccion
         if len(fline) != 0:
-            instruction = ass.process(fline)
-            print(instruction)
+            instruction = ass.divideInstruction(fline)
+            if ass.isComplex(instruction):
+                context = ass.ComplexContext()
+                # Valoramos el tipo de instruccion que es
+                context.setInstruction(ass.analyzeInstruction(instruction))
+                binary = context.toBinary(instruction)
+                for index, ins in enumerate(binary):
+                    oFile.write(ass.formatBinaryInstruction(ins) + '\n')
+                    print("{:16}".format(ins), end="")
+                    if index < len(binary) - 1:
+                        print(",", end=" ")
+                    else:
+                        print()
+            else:
+                instruction = ass.process(instruction)
+                binary = instruction.toBinary()
+                oFile.write(ass.formatBinaryInstruction(binary) + '\n')
+                print(binary)
     
     print("DONE!")
